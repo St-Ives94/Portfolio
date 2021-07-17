a288f37f2230a5d5f92f0ba599e5eaec6325008d
Ivory Smith <smith.ivory94@gmail.com>
2021-07-17 12:22:01 -0500

    updating items

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -43,7 +43,8 @@ const Footer = () => {
             Jacobo Martínez
           </a>
         </p>
-        <p className="footer__text">Photo by <a href="https://unsplash.com/@alex_andrews?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Alexander Andrews</a> on <a href="https://unsplash.com/s/photos/space?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        <p className="footer__text">Photo by <a href="https://unsplash.com/@xanswer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Guillaume Le Louarn</a> on <a href="https://unsplash.com/s/photos/space?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
   </p>
 
         {isEnabled && <GithubButtons />}
