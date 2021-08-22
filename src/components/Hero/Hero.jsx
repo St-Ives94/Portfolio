eee61e1ec8339c766991efa5dc13e2619897de38
Ivory Smith <smith.ivory94@gmail.com>
2021-08-22 13:51:12 -0500

    ...

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             {title || 'Hi, my name is'}{' '}
             <span className="text-color-main">{name}</span>
             <br />
-            {subtitle}
+            {subtitle }
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
